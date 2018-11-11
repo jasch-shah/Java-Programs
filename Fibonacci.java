abf90b53ec35cafc44cb612e938371bfcb8d9d8c
Jash Shah <jash.shah20@gmail.com>
2018-11-11 21:31:51 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonacci {
+	public static void main(String[] args) {
+		int febCount = 15;
+		int[] feb = new int[febCount];
+		feb[0]=0;
+		feb[1]=1;
+		for(int i=2;i<febCount;i++)
+			feb[i]=feb[i-1]+feb[i-2];
+
+		for(int i=0;i<febCount;i++)
+			System.out.print(feb[i]+" ");
+		
+	}
+}
